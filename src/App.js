cd677dd5c650c1e2ef9741698fd65e9cb132d96d
Nina Proctor <nina.proctor@mykenzie.snhu.edu>
2023-01-03 23:14:42 -0500

    Functionality working for chat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
+
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    setSocket(io("http://localhost:4000"));
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on("message-from-server", (data) => {
+      // setChat([...chat, data.message])
+      setChat((prev) => [...prev, data.message]);
+      // console.log("message recieved", data);
+    });
+  }, [socket]);
+
+  function handleForm(e) {
+    e.preventDefault();
+    console.log(message);
+    socket.emit("send-message", { message });
+    setMessage("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Box sx={{marginBottom:5}}>
+          {chat.map((message) => (
+            <Typography key={message}>{message}</Typography>
+          ))}
+        </Box>
+        <Box component="form" onSubmit={handleForm}>
+          <TextField
+            size="small"
+            id="standard-basic"
+            label="Send a message"
+            variant="standard"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button varient="text" type="submit">
+            Send
+          </Button>
+        </Box>
+      </Container>
     </div>
   );
 }
